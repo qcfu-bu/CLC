7f33498159c4ea80200ed15bc41deafe8ae7f2cd
Qiancheng Fu <qcfu@bu.edu>
2021-05-31 18:34:43 -0400

    M3

--- M3/bin/main.ml
+++ M3/bin/main.ml
@@ -12,22 +12,30 @@ let x = new_var (fun x -> Var x) "x"
 let y = new_var (fun x -> Var x) "y"
 
 let a = new_var (fun x -> Var x) "a"
+let __ = new_var (fun x -> Var x) ""
 
 let ty1 = 
   _Prod _Zero _Type (bind_var a (
-    _Prod _One (_Var a) (bind_var x (
-      _Prod _W (_Var a) (bind_var y (
+    _Prod _One (_Prod _One (_Var a) (bind_var __ (_Var a))) (bind_var x (
+      _Prod _W (_Var a) (bind_var __ (
         _Var a))))))
+
+(* let t1 = 
+  _Lambda (bind_var __ (
+    _Lambda (bind_var f (
+      _Lambda (bind_var a (
+        _App (_Var f) (_Var a))))))) *)
+
 let t1 = 
-  _Lambda (bind_var a (
-    _Lambda (bind_var x (
-      _Lambda (bind_var y (
-        _Var x))))))
+  _Lambda (bind_var __ (
+    _Lambda (bind_var f (
+      _Lambda (bind_var a (
+        _Var a))))))
 
 let t = _Ann t1 ty1
 
 let _ = 
-  let t = unbox (_App (_App (_App t ty1) t1) t1) in
+  let t = unbox t in
   let p = _W in
   let ctx, ty = infer empty p t in
   let t = cbv t in
