20a0ff0cc7a7d3f6c99e6ddaaaa58a00a12ce0db
Qiancheng Fu <qcfu@bu.edu>
2021-06-12 23:26:31 -0400

    M6

--- M6/examples/test3.v
+++ M6/examples/test3.v
@@ -2,50 +2,55 @@
 
 Definition Loc : Type := Nat.
 
-Definition UL : Type := (Unit | Loc).
-Definition nil : Type := (x : UL * Eq(x, left (), UL)).
-Definition cons : Loc -> Type := fun l => (x : UL * Eq(x, right l, UL)).
+Definition UL : Type -> Type := 
+  fun A => (Unit | (A * Loc)).
+Definition nil : Type -> Type := 
+  fun A => (x : UL A * Eq(x, left (), UL A)).
+Definition cons : Type -> Loc -> Type := 
+  fun A l => (x : UL A * (a : A * Eq(x, right (a, l), UL A))).
 
-Definition LList : Nat -> Loc -> Linear := 
-  fun n =>
+Definition LList : Type -> Nat -> Loc -> Linear := 
+  fun A n =>
     iter(
       fun _ => Loc -> Linear, 
-      fun l => [l |-> nil],
-      fun n LListN l => (l' : Loc * ([l |-> cons l'] * (LListN l'))),
+      fun l => [l |-> nil A],
+      fun n LListN l => (l' : Loc * ([l |-> cons A l'] * (LListN l'))),
       n).
 
-Definition List : (n : Nat) -> Linear := 
-  fun n => (l : Loc * LList n l).
+Definition List : Type -> (n : Nat) -> Linear := 
+  fun A n => (l : Loc * LList A n l).
  
-Definition Nil : Unit -> List 0 := 
-  fun _ => alloc nil (left (), refl(left (), UL)).
+Definition Nil : (A : Type) -> List A 0 := 
+  fun A => alloc (nil A) (left (), refl(left (), UL A)).
 
-Definition Cons : (n : Nat) -> List n -> List (S n) :=
-  fun _ ls =>
+Definition Cons : (A : Type) -> A -> (n : Nat) -> List A n -> List A (S n) :=
+  fun A a _ ls =>
     let (l1, ls) := ls in
-    let (l2, c) := alloc (cons l1) (right l1, refl(right l1, UL)) in
+    let (l2, c) := alloc (cons A l1) 
+      (right (a, l1), (a, refl(right (a, l1), UL A))) 
+    in
     (l2, (l1, (c, ls))).
 
-Definition Uncons : (n : Nat) -> List (S n) -> List n := 
-  fun _ ls => 
+Definition Uncons : (A : Type) -> (n : Nat) -> List A (S n) -> List A n := 
+  fun A _ ls => 
     let (l2, ls) := ls in
     let (l1, ls) := ls in
     let (c, ls) := ls in
-    let _ := free (cons l1) (l2, c) in
+    let _ := free (cons A l1) (l2, c) in
     (l1, ls).
 
-Definition MakeList : (n : Nat) -> List n := 
-  fun n => 
-    iter(fun n => List n, Nil (), fun n lsN => Cons n lsN, n).
+Definition MakeList : (A : Type) -> A -> (n : Nat) -> List A n := 
+  fun A a n => 
+    iter(fun n => List A n, Nil A, fun n lsN => Cons A a n lsN, n).
 
-Definition FreeList : (n : Nat) -> List n -> Unit := 
-  fun n => 
+Definition FreeList : (A : Type) -> (n : Nat) -> List A n -> Unit := 
+  fun A n => 
     iter(
-      fun n => List n -> Unit,
-      fun ls => free nil ls,
+      fun n => List A n -> Unit,
+      fun ls => free (nil A) ls,
       fun n FreeN ls =>
-        let ls := Uncons n ls in
+        let ls := Uncons A n ls in
         FreeN ls,
       n).
 
-Definition main : List 5 := MakeList 5.
+Definition main : Unit := ().
--- M6/log.v
+++ M6/log.v
@@ -1,100 +1,72 @@
 checking
 t  :=
   let Loc := (Nat : Type) in
-  let UL := ((Unit | Loc) : Type) in
-  let nil := ((x : UL * Eq(x, (left ()))) : Type) in
-  let cons := (fun l => (x : UL * Eq(x, (right l))) : Loc -> Type) in
-  let LList :=
-    (fun n =>
-       iter(fun _ => Loc -> Linear, fun l => [l |-> nil],
-         fun n =>
-           fun LListN =>
-             fun l => (l' : Loc * ([l |-> (cons) l'] * (LListN) l')),
-         n) :
-      Nat -> Loc -> Linear)
+  let UL := (fun A => (Unit | (A * Loc)) : Type -> Type) in
+  let nil := (fun A => (x : (UL) A * Eq(x, (left ()))) : Type -> Type) in
+  let cons :=
+    (fun A => fun l => (x : (UL) A * (a : A * Eq(x, (right (a, l))))) :
+      Type -> Loc -> Type)
   in
-  let List := (fun n => (l : Loc * ((LList) n) l) : (n : Nat) -> Linear) in
+  let LList :=
+    (fun A =>
+       fun n =>
+         iter(fun _ => Loc -> Linear, fun l => [l |-> (nil) A],
+           fun n =>
+             fun LListN =>
+               fun l => (l' : Loc * ([l |-> ((cons) A) l'] * (LListN) l')),
+           n) :
+      Type -> Nat -> Loc -> Linear)
+  in
+  let List :=
+    (fun A => fun n => (l : Loc * (((LList) A) n) l) :
+      Type -> (n : Nat) -> Linear)
+  in
   let Nil :=
-    (fun _ => ((alloc) nil) ((left ()), refl((left ()),  UL)) :
-      Unit -> (List) 0)
+    (fun A => ((alloc) (nil) A) ((left ()), refl((left ()),  (UL) A)) :
+      (A : Type) -> ((List) A) 0)
   in
   let Cons :=
-    (fun _ =>
-       fun ls =>
-         let (l1, ls) := ls in
-         let (l2, c) :=
-           ((alloc) (cons) l1) ((right l1), refl((right l1),  UL))
-         in (l2, (l1, (c, ls))) :
-      (n : Nat) -> (List) n -> (List) (n +1))
+    (fun A =>
+       fun a =>
+         fun _ =>
+           fun ls =>
+             let (l1, ls) := ls in
+             let (l2, c) :=
+               ((alloc) ((cons) A) l1)
+                 ((right (a, l1)), (a, refl((right (a, l1)),  (UL) A)))
+             in (l2, (l1, (c, ls))) :
+      (A : Type) -> A -> (n : Nat) -> ((List) A) n -> ((List) A) (n +1))
   in
   let Uncons :=
-    (fun _ =>
-       fun ls =>
-         let (l2, ls) := ls in
-         let (l1, ls) := ls in
-         let (c, ls) := ls in let _ := ((free) (cons) l1) (l2, c) in (l1, ls) :
-      (n : Nat) -> (List) (n +1) -> (List) n)
+    (fun A =>
+       fun _ =>
+         fun ls =>
+           let (l2, ls) := ls in
+           let (l1, ls) := ls in
+           let (c, ls) := ls in
+           let _ := ((free) ((cons) A) l1) (l2, c) in (l1, ls) :
+      (A : Type) -> (n : Nat) -> ((List) A) (n +1) -> ((List) A) n)
   in
   let MakeList :=
-    (fun n =>
-       iter(fun n => (List) n, (Nil) (), fun n => fun lsN => ((Cons) n) lsN,
-         n) :
-      (n : Nat) -> (List) n)
+    (fun A =>
+       fun a =>
+         fun n =>
+           iter(fun n => ((List) A) n, (Nil) A,
+             fun n => fun lsN => ((((Cons) A) a) n) lsN, n) :
+      (A : Type) -> A -> (n : Nat) -> ((List) A) n)
   in
   let FreeList :=
-    (fun n =>
-       iter(fun n => (List) n -> Unit, fun ls => ((free) nil) ls,
-         fun n =>
-           fun FreeN => fun ls => let ls := ((Uncons) n) ls in (FreeN) ls,
-         n) :
-      (n : Nat) -> (List) n -> Unit)
-  in let main := ((MakeList) 5 : (List) 5) in main
+    (fun A =>
+       fun n =>
+         iter(fun n => ((List) A) n -> Unit, fun ls => ((free) (nil) A) ls,
+           fun n =>
+             fun FreeN =>
+               fun ls => let ls := (((Uncons) A) n) ls in (FreeN) ls,
+           n) :
+      (A : Type) -> (n : Nat) -> ((List) A) n -> Unit)
+  in let main := (() : Unit) in main
 complete
 post_ctx := {
 }
-t  :=
-  let (l1, ls) :=
-    let (l1, ls) :=
-      let (l1, ls) :=
-        let (l1, ls) :=
-          let (l1, ls) :=
-            ((alloc) (x : (Unit | Nat) * Eq(x, (left ()))))
-              ((left ()), refl((left ()),  (Unit | Nat)))
-          in
-          let (l2, c) :=
-            ((alloc) (x : (Unit | Nat) * Eq(x, (right l1))))
-              ((right l1), refl((right l1),  (Unit | Nat)))
-          in (l2, (l1, (c, ls)))
-        in
-        let (l2, c) :=
-          ((alloc) (x : (Unit | Nat) * Eq(x, (right l1))))
-            ((right l1), refl((right l1),  (Unit | Nat)))
-        in (l2, (l1, (c, ls)))
-      in
-      let (l2, c) :=
-        ((alloc) (x : (Unit | Nat) * Eq(x, (right l1))))
-          ((right l1), refl((right l1),  (Unit | Nat)))
-      in (l2, (l1, (c, ls)))
-    in
-    let (l2, c) :=
-      ((alloc) (x : (Unit | Nat) * Eq(x, (right l1))))
-        ((right l1), refl((right l1),  (Unit | Nat)))
-    in (l2, (l1, (c, ls)))
-  in
-  let (l2, c) :=
-    ((alloc) (x : (Unit | Nat) * Eq(x, (right l1))))
-      ((right l1), refl((right l1),  (Unit | Nat)))
-  in (l2, (l1, (c, ls)))
-ty :=
-  (l : Nat *
-    (l' : Nat *
-      ([l |-> (x : (Unit | Nat) * Eq(x, (right l')))] *
-        (l'0 : Nat *
-          ([l' |-> (x : (Unit | Nat) * Eq(x, (right l'0)))] *
-            (l'1 : Nat *
-              ([l'0 |-> (x : (Unit | Nat) * Eq(x, (right l'1)))] *
-                (l'2 : Nat *
-                  ([l'1 |-> (x : (Unit | Nat) * Eq(x, (right l'2)))] *
-                    (l'3 : Nat *
-                      ([l'2 |-> (x : (Unit | Nat) * Eq(x, (right l'3)))] *
-                        [l'3 |-> (x : (Unit | Nat) * Eq(x, (left ())))])))))))))))
+t  := ()
+ty := Unit
