92abe6e470670453dbc9ac5f040581eb2918d945
Qiancheng Fu <qcfu@bu.edu>
2022-03-16 08:50:43 -0400

    cclc

--- coq/cclc/cclc_soundness.v
+++ coq/cclc/cclc_soundness.v
@@ -4,3 +4,49 @@ Require Import AutosubstSsr ARS
   clc_context clc_ast clc_confluence clc_subtype clc_typing
   clc_weakening clc_substitution clc_inversion clc_validity
   cclc_ast cclc_dual cclc_typing.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Lemma congr0_sym p q : congr0 p q -> congr0 q p.
+Proof.
+  elim; move=>*; eauto using congr0.
+Qed.
+
+Lemma cong0_type Γ p q : 
+  ok Γ -> Γ ⊢ p -> congr0 p q -> Γ ⊢ q.
+Proof with eauto using cclc_type, congr0.
+  move=>wf ty. elim: ty wf q=>{Γ p}.
+  move=>Γ m A s tym wf q cr. inv cr.
+  move=>Γ1 Γ2 Γ p q mrg typ ihp tyq ihq wf q0 cr. 
+  have[wf1 wf2]:=merge_context_ok_inv mrg wf. inv cr.
+  { econstructor.
+    apply: merge_sym...
+    all: eauto. }
+  { inv tyq.
+    have[G[mrg1 mrg2]]:=merge_splitL (merge_sym mrg) H1.
+    econstructor. 
+    exact: mrg2.
+    econstructor.
+    exact: (merge_sym mrg1).
+    all: eauto. }
+  { inv typ.
+    have[G[mrg1 mrg2]]:=merge_splitR mrg H1.
+    econstructor. 
+    exact: mrg2.
+    exact: H3.
+    econstructor.
+    exact: mrg1.
+    all: eauto. }
+  { inv typ.
+    econstructor.
+    exact: H0.
+    have mrg': 
+      Ch A.[ren (+1)] :L Ch B :L Γ1 ∘ _: _: Γ2 => 
+      Ch A.[ren (+1)] :L Ch B :L Γ.
+      repeat econstructor; eauto.
+    econstructor...
+    apply strengthen.
+
+  }
--- coq/cclc/cclc_weakening.v
+++ coq/cclc/cclc_weakening.v
@@ -34,3 +34,53 @@ Lemma rename_ok Γ Γ' p ξ :
   Γ ⊢ p -> agree_ren ξ Γ Γ' -> Γ' ⊢ p.[ren ξ].
 Proof.
   move=>tp. elim: tp Γ' ξ=>{Γ p}.
+  move=>Γ m A s tym Γ' ξ agr; asimpl.
+    econstructor.
+    apply: clc_weakening.rename_ok.
+    exact: tym.
+    exact: agr.
+  move=>Γ1 Γ2 Γ p q mrg typ ihp tyq ihq Γ' ξ agr; asimpl.
+    have[G1[G2[mrg1[agr1 agr2]]]]:=merge_agree_ren_inv agr mrg.
+    econstructor.
+    exact: mrg1.
+    apply: ihp.
+    exact: agr1.
+    apply: ihq.
+    exact: agr2.
+  move=>Γ p A B d typ ihp Γ' ξ agr; asimpl.
+    have d':=rename_dual ξ d.
+    econstructor.
+    apply: d'.
+    asimpl.
+    apply: ihp.
+    have: 
+      (agree_ren (upren (upren ξ))
+        (Ch A.[ren (+1)] :L Ch B :L Γ)
+        (Ch A.[ren (+1)].[ren (upren ξ)] :L Ch B.[ren ξ] :L Γ')).
+      econstructor.
+      econstructor.
+      exact:agr.
+    by asimpl.
+Qed.
+
+Lemma weakeningU Γ p A : Γ ⊢ p -> A :U Γ ⊢ p.[ren (+1)].
+Proof with eauto using agree_ren, agree_ren_refl.
+  move=>ty. apply: rename_ok...
+Qed.
+
+Lemma weakeningN Γ p : Γ ⊢ p -> _: Γ ⊢ p.[ren (+1)].
+Proof with eauto using agree_ren, agree_ren_refl.
+  move=>ty. apply: rename_ok...
+Qed.
+
+Lemma eweakeningU Γ p p' A :
+  p' = p.[ren (+1)]%P -> Γ ⊢ p -> A :U Γ ⊢ p'.
+Proof.  
+  move=>*; subst. by apply: weakeningU.
+Qed.
+
+Lemma eweakeningN Γ p p' :
+  p' = p.[ren (+1)]%P -> Γ ⊢ p -> _: Γ ⊢ p'.
+Proof.  
+  move=>*; subst. by apply weakeningN.
+Qed.
