188d318a4b0997833d1f5d508274258ee1eb3484
Qiancheng Fu <qcfu@bu.edu>
2022-04-06 01:41:36 -0400

    cclc

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,20 +1,29 @@
 open Cclc
 open Core
 open Name
+open Term
 
 let id = Id.mk "pair"
 
-let m =
-  Constr (id, [Knd U; Knd L])
+let a = Var.mk "a"
+
+let b = Var.incr 1 (Var.mk "b")
+
+let c = Var.incr 2 (Var.mk "c")
+
+let m = Constr (id, [ Var a; Var b; Var c ])
 
 let x = Var.mk "x"
 
 let y = Var.mk "y"
 
-let p = PConstr (id, [x; y])
+let z = Var.mk "z"
+
+let p = PConstr (id, [ x; y; z ])
 
 let n =
-  let y = Var.map ((+) 1) y in
-  Match (m, Mot0, [(p, App (Var x, Var y))])
+  let y = Var.incr 1 y in
+  let z = Var.incr 2 z in
+  Match (m, Mot0, [ (p, App (Var z, App (Var x, Var y))) ])
 
 let w = whnf n
--- src/lib/name.ml
+++ src/lib/name.ml
@@ -1,66 +1,96 @@
 open Format
 
-module Meta :
-sig
+module Meta : sig
   type t
+
   val mk : unit -> t
+
   val equal : t -> t -> bool
+
   val pp : formatter -> t -> unit
-end =
-struct
+end = struct
   type t = int
+
   let stamp = ref 0
+
   let mk () =
     let i = !stamp in
     let _ = incr stamp in
     i
+
   let equal i1 i2 = i1 = i2
+
   let pp fmt i = fprintf fmt "?%d" i
 end
 
-module Var :
-sig
+module Var : sig
   type t
+
   val mk : string -> t
+
   val equal : t -> t -> bool
+
   val compare : t -> t -> int
+
   val map : (int -> int) -> t -> t
+
+  val incr : int -> t -> t
+
   val case : t -> (t -> 'a) -> (t -> 'a) -> 'a
+
   val pp : formatter -> t -> unit
+
   val pp_debug : formatter -> t -> unit
-end =
-struct
+end = struct
   type t = string * int
+
   let mk s = (s, 0)
+
   let equal (_, i1) (_, i2) = i1 = i2
+
   let compare (_, i1) (_, i2) = Int.compare i1 i2
+
   let map f (s, i) = (s, max 0 (f i))
+
+  let incr n (s, i) = (s, i + n)
+
   let case (s, i) f g =
     match i with
     | 0 -> f (s, 0)
     | _ -> g (s, i - 1)
+
   let pp fmt (s, _) = fprintf fmt "%s" s
+
   let pp_debug fmt (s, i) = fprintf fmt "%s#%d" s i
 end
 
-module Id :
-sig
+module Id : sig
   type t
+
   val mk : string -> t
+
   val equal : t -> t -> bool
+
   val compare : t -> t -> int
+
   val pp : formatter -> t -> unit
+
   val pp_debug : formatter -> t -> unit
-end =
-struct
+end = struct
   type t = string * int
+
   let stamp = ref 0
+
   let mk s =
     let i = !stamp in
     let _ = incr stamp in
     (s, i)
+
   let equal (_, i1) (_, i2) = i1 = i2
+
   let compare (_, i1) (_, i2) = Int.compare i1 i2
+
   let pp fmt (s, _) = fprintf fmt "%s" s
+
   let pp_debug fmt (s, i) = fprintf fmt "%s#%d" s i
 end
