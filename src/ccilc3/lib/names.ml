2459072eb1a46ef378d2a9a4c50d73be01cb0d2a
qcfu <qcfu@bu.edu>
2022-07-13 23:14:29 -0400

    ccilc3

---
+++ src/ccilc3/lib/names.ml
@@ -0,0 +1,103 @@
+open Fmt
+
+module V : sig
+  type t
+
+  val mk : string -> t
+  val bind : int -> t
+  val equal : t -> t -> bool
+  val compare : t -> t -> int
+  val freshen : t -> t
+  val is_bound : t -> int -> int -> int option
+  val pp : Format.formatter -> t -> unit
+end = struct
+  type t =
+    | Free of string * int
+    | Bound of int
+
+  let stamp = ref 0
+
+  let mk s =
+    incr stamp;
+    Free (s, !stamp)
+
+  let bind k = Bound k
+
+  let equal x y =
+    match (x, y) with
+    | Free (_, x), Free (_, y) -> x = y
+    | Bound x, Bound y -> x = y
+    | _ -> false
+
+  let compare x y = compare x y
+
+  let freshen x =
+    match x with
+    | Bound _ -> x
+    | Free (x, _) ->
+      incr stamp;
+      Free (x, !stamp)
+
+  let is_bound x sz k =
+    match x with
+    | Bound i ->
+      if k <= i && i < k + sz then
+        Some i
+      else
+        None
+    | Free _ -> None
+
+  let pp fmt x =
+    match x with
+    | Bound x -> pf fmt "_%d" x
+    | Free (x, id) -> pf fmt "%s_%d" x id
+end
+
+module D : sig
+  type t
+
+  val mk : string -> t
+  val equal : t -> t -> bool
+  val compare : t -> t -> int
+  val pp : Format.formatter -> t -> unit
+end = struct
+  type t = string * int
+
+  let stamp = ref 0
+
+  let mk s =
+    incr stamp;
+    (s, !stamp)
+
+  let equal x y = snd x = snd y
+  let compare x y = compare (snd x) (snd y)
+  let pp fmt (s, id) = pf fmt "%s_d%d" s id
+end
+
+module C : sig
+  type t
+
+  val mk : string -> t
+  val equal : t -> t -> bool
+  val compare : t -> t -> int
+  val pp : Format.formatter -> t -> unit
+end = struct
+  type t = string * int
+
+  let stamp = ref 0
+
+  let mk s =
+    incr stamp;
+    (s, !stamp)
+
+  let equal x y = snd x = snd y
+  let compare x y = compare (snd x) (snd y)
+  let pp fmt (s, id) = pf fmt "%s_c%d" s id
+end
+
+module VSet = Set.Make (V)
+module CSet = Set.Make (C)
+module DSet = Set.Make (D)
+module VMap = Map.Make (V)
+module CMap = Map.Make (C)
+module DMap = Map.Make (D)
